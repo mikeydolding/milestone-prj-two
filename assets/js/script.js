e2a68382efbe66f28aed870f13bb8b8baa4e9504
tekkoop <tekkoop@tutanota.com>
2023-07-08 16:21:54 +0300

    Edited scripts.js to displayed answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 score = 0;
+answers = [];
+counter = 0;
+const results = document.getElementById("answers");
+const questionum = document.getElementById("questionnumber");
+const counternum = document.getElementById("counternumber");
+
+
 function change_score() {
   alert("correct answer  " + questions[counter].correct.toString());
   alert("user answer is " + userAnswer.toString());
@@ -10,30 +17,45 @@ function change_score() {
 }
 
 function lastpage() {
+  for (let i = 0; i < counter.length; i++) {
+    results.innerHTML = answers[i];
+  }
   let replacement = "<h1> Quiz completed </h1>";
   let display = document.querySelector("#content");
   display.innerHTML = replacement;
+
 }
 
 function startagain() {
   window.location.href = "index.html";
+  answers = [];
+  counter = 0;
+  results.innerHTML = "";
 }
 function displaynextquestion() {
   counter++;
   if (counter == questions.length) {
     lastpage();
   }
-  if (counter < questions.length - 1) counter++;
+  if (counter < questions.length) 
   displayquestion();
+  results.innerHTML = answers;
+  questionum.innerHTML = questions[counter].Type
+  counternum.innerHTML = counter + 1
+
 }
 function displaypreviousquestion() {
   if (counter > 0) counter--;
   displayquestion();
+  questionum.innerHTML = questions[counter].Type
+  counternum.innerHTML = counter + 1
+
 }
 
 function displayfirstquestion() {
-  counter = 0;
   displayquestion();
+  questionum.innerHTML = questions[counter].Type
+  counternum.innerHTML = counter + 1
 }
 
 function checkanswer() {
@@ -58,14 +80,17 @@ function checkanswer() {
   }
 }
 
-function prevquestion() {
-  counter = counter - 1;
-}
+//function prevquestion() {
+//  counter = counter - 1;
+//}
 
 function recordanswer(element, user_choice) {
-  alert("answer= " + user_choice);
-
+  answers[counter] = user_choice;
+  for (let i = 0; i < counter.length; i++) {
+    results.innerHTML = answers[i];
+  }
 }
+
 function displayquestion() {
   console.log("number of questions=" + questions.length);
   let display = document.querySelector("#content");
@@ -74,16 +99,16 @@ function displayquestion() {
       '<div class="item"></div> <div class="item"> </div> <div class="item"> ' +
       questions[counter].Question +
       " </div>" +
-      '<div class="item"> A </div> <div class="item" > <input name ="ques" type="checkbox" onclick="recordanswer(this,1)"> </div> <div class="item">' +
+      '<div class="item"> A </div> <div class="item" > <input name ="ques" type="checkbox" onclick="recordanswer(this,questions[counter].a)"> </div> <div class="item">' +
       questions[counter].a +
       " </div>" +
-      '<div class="item"> B </div> <div class="item"> <input name ="ques" type="checkbox" onclick="recordanswer(this,2)"> </div> <div class="item">' +
+      '<div class="item"> B </div> <div class="item"> <input name ="ques" type="checkbox" onclick="recordanswer(this,questions[counter].b)"> </div> <div class="item">' +
       questions[counter].b +
       " </div>" +
-      '<div class="item"> C </div> <div class="item ""> <input name ="ques" type="checkbox" onclick="recordanswer(this,3)"> </div> <div class="item">' +
+      '<div class="item"> C </div> <div class="item ""> <input name ="ques" type="checkbox" onclick="recordanswer(this,questions[counter].c)"> </div> <div class="item">' +
       questions[counter].c +
       "</div>" +
-      '<div class= "item"> D </div> <div class="item" > <input name ="ques" type="checkbox" onclick="recordanswer(this,4)"> </div> <div class="item">' +
+      '<div class= "item"> D </div> <div class="item" > <input name ="ques" type="checkbox" onclick="recordanswer(this,questions[counter].d)"> </div> <div class="item">' +
       questions[counter].d +
       "</div>";
 
@@ -92,21 +117,17 @@ function displayquestion() {
 
   if (questions[counter].Type == 2) {
     let display = document.querySelector("#content");
-
     let replacement =
       '<div class="item"> </div> <div class="item"> </div> <div class="item">' +
       questions[counter].Question +
       "</div>" +
-      '<div class="item"> A </div> <div class="item" > <input name ="ques" type="radio" onclick="recordanswer(this,1)"> </div> <div class="item">' +
+      '<div class="item"> A </div> <div class="item" > <input name ="ques" type="radio" onclick="recordanswer(this,questions[counter].a)"> </div> <div class="item">' +
       questions[counter].a +
       " </div>" +
-      '<div class="item"> B </div> <div class="item"> <input name ="ques" type="radio" onclick="recordanswer(this,2)"> </div> <div class="item">' +
+      '<div class="item"> B </div> <div class="item"> <input name ="ques" type="radio" onclick="recordanswer(this,questions[counter].b)"> </div> <div class="item">' +
       questions[counter].b +
       " </div>";
     display.innerHTML = replacement;
-    console.log("questions[counter].Question" + questions[counter].Question);
-    console.log("counter=" + counter);
-    console.log("questions[counter].a=" + questions[counter].a);
   }
 
   if (questions[counter].Type == 3) {
@@ -114,16 +135,16 @@ function displayquestion() {
       '<div class="item"> </div> <div class="item"> </div> <div class="item"> ' +
       questions[counter].Question +
       "  </div>" +
-      '<div class="item"> A </div> <div class="item" > <input name ="ques" type="radio" onclick="recordanswer(this,1)"> </div> <div class="item">' +
+      '<div class="item"> A </div> <div class="item" > <input name ="ques" type="radio" onclick="recordanswer(this,questions[counter].a)"> </div> <div class="item">' +
       questions[counter].a +
       " </div>" +
-      '<div class="item"> B </div> <div class="item"> <input name ="ques" type="radio" onclick="recordanswer(this,2)"> </div> <div class="item">' +
+      '<div class="item"> B </div> <div class="item"> <input name ="ques" type="radio" onclick="recordanswer(this,questions[counter].b)"> </div> <div class="item">' +
       questions[counter].b +
       " </div>" +
-      '<div class="item"> C </div> <div class="item ""> <input name ="ques" type="radio" onclick="recordanswer(this,3)"> </div> <div class="item">' +
+      '<div class="item"> C </div> <div class="item ""> <input name ="ques" type="radio" onclick="recordanswer(this,questions[counter].c)"> </div> <div class="item">' +
       questions[counter].c +
       "</div>" +
-      '<div class= "item"> D </div> <div class="item" > <input name ="ques" type="radio" onclick="recordanswer(this,4)"> </div> <div class="item">' +
+      '<div class= "item"> D </div> <div class="item" > <input name ="ques" type="radio" onclick="recordanswer(this,questions[counter].d)"> </div> <div class="item">' +
       questions[counter].d +
       "</div>";
     display.innerHTML = replacement;
